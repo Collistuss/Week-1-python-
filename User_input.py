c1ff9036ccef80605a4eecad7d8a34f49c120283
Collistuss <kibecollistus@gmail.com>
2024-11-09 19:08:06 +0300

    User_input.py
    Done

---
+++ User_input.py
@@ -0,0 +1,10 @@
+# Create a new file named user_input.py
+# Save the code below in that file.
+
+# Asking for user inputs
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where do you live? ")
+
+# Displaying the personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
